75da7cf9a2d35135cbe33f6e00a8b0f4360a6cf3
Shaw <shshaw@gmail.com>
2014-06-12 09:44:42 -0500

    Comment consistency

--- loadCSS.js
+++ loadCSS.js
@@ -15,15 +15,15 @@ function loadCSS( href, before, media ){
 		Arguments explained:
 		- `href` is the optional URL or array of URLs for your CSS file(s).
 		- `before` optionally defines the element we'll use as a reference for injecting our <link>
-				By default, `before` uses the first <script> element in the page.
+				By default, `before` uses the first `<script>` element in the page.
 				However, since the order in which stylesheets are referenced matters, you might need a more specific location in your document.
 				If so, pass a different reference element to the `before` argument and it'll insert before that instead
-		- `media` is the value for the <link>'s media attribute. Default is "all" or if using the <noscript> option, whatever the original's attribute is.
+		- `media` is the value for the `<link>`'s media attribute. Default is "all" or if using the `<noscript>` option, whatever the original's attribute is.
 	
 		Note: `insertBefore` is used instead of `appendChild`, for safety re: http://www.paulirish.com/2011/surefire-dom-element-insertion/
 	*/
 	
-	// Create a <link> element
+	// Create a `<link>` element
 	function makeLink(href) {
 		var ss = window.document.createElement( "link" );
 		ss.rel = "stylesheet";
@@ -31,7 +31,7 @@ function loadCSS( href, before, media ){
 		return ss;
 	}
 	
-	// Inject <link> into DOM
+	// Inject `<link>` into DOM
 	function injectLink(ss,ref) {
 		// Save a reference to the original media attribute, use the `media` argument, or default to "all"
 		var ssMedia = ss.media || media || "all";
@@ -57,26 +57,26 @@ function loadCSS( href, before, media ){
 			injectLink(makeLink(href),ref);
 		}
 	} else {
-		// Otherwise, find all `noscript` elements with the class `async-css` and pull any `link` elements from there
+		// Otherwise, find all `noscript` elements with the class `async-css` and pull any `<link>` elements from there
 		var noscripts = window.document.getElementsByTagName("noscript");
 	
 		if ( noscripts.length ) {
-			// Create a dummy element to drop all of the `noscript`s contents into to check for `link` elements 
+			// Create a dummy element to drop all of the `<noscript>`s contents into to check for `<link>` elements 
 			var el = window.document.createElement('div');
 			
 			for (i = 0; i < noscripts.length; i++ ) {
-				// Make sure this `noscript` has the `loadCSS` class and has not already been loaded
+				// Make sure this `<noscript>` has the `loadCSS` class and has not already been loaded
 				if ( ( " "+noscripts[i].className+" ").indexOf(' loadCSS ') > -1 && ( " "+noscripts[i].className+" ").indexOf(' loaded ') === -1 ) {
 					el.innerHTML += " " + noscripts[i].innerText;
 					noscripts[i].className += " loaded ";
 				}
 			}
 			
-			// Extract the link elements from our dummy element, `el`
+			// Extract the `<link>` elements from our dummy element, `el`
 			var links = el.getElementsByTagName("link");
 			
 			for (i = 0; i < links.length; i++ ) {
-				// Clone the link element to prevent issues with skipped elements
+				// Clone the `<link>` element to prevent issues with skipped elements
 				var link = links[i].cloneNode(true);
 				injectLink(link,ref);
 			}

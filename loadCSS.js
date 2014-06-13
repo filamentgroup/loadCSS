8efb0103c9db191260c50212689c057d22df7d40
Adam McArthur <adam@adammcarthur.net>
2014-06-14 00:23:47 +1000

    fix formatting v2

--- loadCSS.js
+++ loadCSS.js
@@ -3,7 +3,7 @@ loadCSS: load a CSS file asynchronously.
 [c]2014 @scottjehl, Filament Group, Inc.
 Licensed MIT
 */
-  function loadCSS( href, before, media ){
+function loadCSS( href, before, media ){
   "use strict";
   // Arguments explained:
   // `href` is the URL for your CSS file.

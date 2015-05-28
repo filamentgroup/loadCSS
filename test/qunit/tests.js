721931d41bf36c974dbb226f71a36524fc0642d8
Scott Jehl <scottjehl@gmail.com>
2015-05-28 12:47:59 -0500

    Add unit tests with one failing test that will be fixed when #65 lands.
    add test harness with 2 passing tests (cherry-picking from commit a66b1fb262954a05d939889dd5abecda096838dc)
    unit test: cssom increments by 1 (cherry-picking from commit f7f6943f4b4ebcf8a1d9217ba7b88cfaacc93e8a)
    typo (cherry-picking from commit 444eb0ab4efa2bb9690b842223b717a59cf4ec6b)
    unit test: stylesheet successfully loaded (cherry-picking from commit b07aeb1d12971c3c292fac78327e602befdeb23d)
    add passing tests for all other api features including relative paths (cherry-picking from commit 1e8393d91cf54947e54d4d7556fb120583abd692)
    test relative paths better, without onload, so it compares paths (cherry-picking from commit b06476da8b561d8a68abe0fa03672b6c675727a6)
    add test harness with 2 passing tests (cherry-picking from commit a66b1fb262954a05d939889dd5abecda096838dc)
    unit test: cssom increments by 1 (cherry-picking from commit f7f6943f4b4ebcf8a1d9217ba7b88cfaacc93e8a)
    typo (cherry-picking from commit 444eb0ab4efa2bb9690b842223b717a59cf4ec6b)
    unit test: stylesheet successfully loaded (cherry-picking from commit b07aeb1d12971c3c292fac78327e602befdeb23d)
    add passing tests for all other api features including relative paths (cherry-picking from commit 1e8393d91cf54947e54d4d7556fb120583abd692)
    test relative paths better, without onload, so it compares paths (cherry-picking from commit b06476da8b561d8a68abe0fa03672b6c675727a6)
    add test harness with 2 passing tests (cherry-picking from commit a66b1fb262954a05d939889dd5abecda096838dc)
    unit test: cssom increments by 1 (cherry-picking from commit f7f6943f4b4ebcf8a1d9217ba7b88cfaacc93e8a)
    typo (cherry-picking from commit 444eb0ab4efa2bb9690b842223b717a59cf4ec6b)
    unit test: stylesheet successfully loaded (cherry-picking from commit b07aeb1d12971c3c292fac78327e602befdeb23d)
    add passing tests for all other api features including relative paths (cherry-picking from commit 1e8393d91cf54947e54d4d7556fb120583abd692)
    test relative paths better, without onload, so it compares paths (cherry-picking from commit b06476da8b561d8a68abe0fa03672b6c675727a6)

---
+++ test/qunit/tests.js
@@ -0,0 +1,86 @@
+/*global window:true*/
+(function(window) {
+	/*
+		======== A Handy Little QUnit Reference ========
+		http://api.qunitjs.com/
+
+		Test methods:
+			module(name, {[setup][ ,teardown]})
+			test(name, callback)
+			expect(numberOfAssertions)
+			stop(increment)
+			start(decrement)
+		Test assertions:
+			ok(value, [message])
+			equal(actual, expected, [message])
+			notEqual(actual, expected, [message])
+			deepEqual(actual, expected, [message])
+			notDeepEqual(actual, expected, [message])
+			strictEqual(actual, expected, [message])
+			notStrictEqual(actual, expected, [message])
+			throws(block, [expected], [message])
+	*/
+
+	// initial value of media attr
+	var initialMedia = "only x";
+
+	test( 'function loadCSS exists', function(){
+		expect(2);
+		ok( window.loadCSS, "loadCSS should exist on the window object" );
+		ok( typeof window.loadCSS === "function", "loadCSS should be a function" );
+	});
+
+	test( 'loadCSS adds a stylesheet to CSSOM', function(){
+		expect(1);
+		var omLength = window.document.styleSheets.length;
+		loadCSS("files/test.css");
+		ok(window.document.styleSheets.length > omLength, "stylesheets incremented" );
+	});
+
+	asyncTest( 'loadCSS loads a CSS file, deprecated callback works as expected (using onload)', function(){
+		expect(1);
+		loadCSS("files/test.css", null, null, function(){
+			ok("stylesheet loaded successfully");
+			start();
+		});
+	});
+
+	asyncTest( 'loadCSS loads a CSS file with a relative path', function(){
+		expect(1);
+		var ss = loadCSS("../../test/qunit/files/test.css");
+		ss.onloadcssdefined(function(){
+			ok("stylesheet loaded successfully");
+			start();
+		});
+	});
+
+	asyncTest( 'loadCSS sets media type before and after the stylesheet is loaded', function(){
+		expect(2);
+		var ss = loadCSS("files/test.css");
+		ok(ss.media, initialMedia, "media type begins as" + initialMedia );
+		ss.onload = function(){
+			equal(this.media, "all", "media type is all");
+			start();
+		};
+	});
+
+	asyncTest( 'loadCSS sets media type to a custom value if specified, after load', function(){
+		expect(2);
+		var med = "print";
+		var ss = loadCSS("files/test.css", null, med);
+		ok(ss.media, initialMedia, "media type begins as " + initialMedia );
+		ss.onload = function(){
+			equal(this.media, med, "media type is " + med);
+			start();
+		};
+	});
+
+	test( 'loadCSS injects before a particular specified element', function(){
+		expect(1);
+		var elem = window.document.getElementById("before-test");
+		var ss = loadCSS("files/test.css", elem);
+		equal(ss.nextElementSibling, elem );
+	});
+
+
+}(window));

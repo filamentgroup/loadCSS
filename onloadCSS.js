890e69b4b4ea04f776687ca5cff621764aec776e
Scott Jehl <scottjehl@gmail.com>
2015-08-28 15:21:38 -0500

    update onloadcss script to use iffe pattern and strict

--- onloadCSS.js
+++ onloadCSS.js
@@ -4,23 +4,24 @@ onloadCSS: adds onload support for asynchronous stylesheets loaded with loadCSS.
 Licensed MIT
 */
 
-/* global navigator */
+(function(w){
+	"use strict";
+	/* exported onloadCSS */
+	w.onloadCSS = function( ss, callback ) {
+		if( "addEventListener" in w ){
+			ss.addEventListener( "load", callback );
+		}
 
-/* exported onloadCSS */
-function onloadCSS( ss, callback ) {
-	if( "addEventListener" in window ){
-		ss.addEventListener( "load", callback );
-	}
+		// This code is for browsers that don’t support onload, any browser that
+		// supports onload should use that instead.
+		// No support for onload:
+		//	* Android 4.3 (Samsung Galaxy S4, Browserstack)
+		//	* Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
+		//	* Android 2.3 (Pantech Burst P9070)
 
-	// This code is for browsers that don’t support onload, any browser that
-	// supports onload should use that instead.
-	// No support for onload:
-	//	* Android 4.3 (Samsung Galaxy S4, Browserstack)
-	//	* Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
-	//	* Android 2.3 (Pantech Burst P9070)
-
-	// Weak inference targets Android < 4.4
-	if( "isApplicationInstalled" in navigator && "onloadcssdefined" in ss ) {
-		ss.onloadcssdefined( callback );
-	}
-}
+		// Weak inference targets Android < 4.4
+		if( "isApplicationInstalled" in w.navigator && "onloadcssdefined" in ss ) {
+			ss.onloadcssdefined( callback );
+		}
+	};
+}(this));

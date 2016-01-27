b79356e3ebb79c70bbfc2e9bb8aaf3668a16f193
Scott Jehl <scott@filamentgroup.com>
2016-01-27 16:49:30 -0600

    fixes #126

--- src/onloadCSS.js
+++ src/onloadCSS.js
@@ -4,25 +4,17 @@ onloadCSS: adds onload support for asynchronous stylesheets loaded with loadCSS.
 Licensed MIT
 */
 
-/* global navigator */
 /* exported onloadCSS */
 function onloadCSS( ss, callback ) {
-	ss.onload = function() {
-		ss.onload = null;
-		if( callback ) {
-			callback.call( ss );
-		}
-	};
-
-	// This code is for browsers that don’t support onload, any browser that
-	// supports onload should use that instead.
-	// No support for onload:
-	//	* Android 4.3 (Samsung Galaxy S4, Browserstack)
-	//	* Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
-	//	* Android 2.3 (Pantech Burst P9070)
-
-	// Weak inference targets Android < 4.4
-	if( "isApplicationInstalled" in navigator && "onloadcssdefined" in ss ) {
-		ss.onloadcssdefined( callback );
+	var called;
+	function newcb(){
+			if( !called && callback ){
+				called = true;
+				callback.call( ss );
+			}
 	}
+	if( ss.addEventListener ){
+		ss.addEventListener( "load", newcb );
+	}
+	ss.onloadcssdefined( newcb );
 }

a4a34353c8fd8db93fa1b4c1ad487ea1b5df77f7
Scott Jehl <scott@filamentgroup.com>
2016-01-27 17:03:16 -0600

    use attachEvent too here, so we can isolate onloadcssdefined

--- src/onloadCSS.js
+++ src/onloadCSS.js
@@ -3,7 +3,7 @@ onloadCSS: adds onload support for asynchronous stylesheets loaded with loadCSS.
 [c]2014 @zachleat, Filament Group, Inc.
 Licensed MIT
 */
-
+/* global navigator */
 /* exported onloadCSS */
 function onloadCSS( ss, callback ) {
 	var called;
@@ -16,5 +16,18 @@ function onloadCSS( ss, callback ) {
 	if( ss.addEventListener ){
 		ss.addEventListener( "load", newcb );
 	}
-	ss.onloadcssdefined( newcb );
+	if( ss.attachEvent ){
+		ss.attachEvent( "onload", newcb );
+	}
+	
+	// This code is for browsers that don’t support onload
+	// No support for onload (it'll bind but never fire):
+	//	* Android 4.3 (Samsung Galaxy S4, Browserstack)
+	//	* Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
+	//	* Android 2.3 (Pantech Burst P9070)
+
+	// Weak inference targets Android < 4.4
+ 	if( "isApplicationInstalled" in navigator ) {
+		ss.onloadcssdefined( newcb );
+	}
 }

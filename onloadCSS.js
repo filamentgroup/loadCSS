22efa3e9b62e8907b59fccbcfbb8eb4ee4c75c0d
Jim Doyle <jimdoyle82@gmail.com>
2016-02-24 22:33:48 +1100

    Added PhantomJS to list of browsers not supporting "onload".

--- onloadCSS.js
+++ onloadCSS.js
@@ -19,10 +19,12 @@ function onloadCSS( ss, callback ) {
 	// No support for onload:
 	//	* Android 4.3 (Samsung Galaxy S4, Browserstack)
 	//	* Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
-	//	* Android 2.3 (Pantech Burst P9070)
+    //	* Android 2.3 (Pantech Burst P9070)
+    //  * PhantomJS (Issue ref: https://github.com/ariya/phantomjs/issues/12332, https://github.com/dimaxweb/CSSLoader/issues/4)
 
 	// Weak inference targets Android < 4.4
-	if( "isApplicationInstalled" in navigator && "onloadcssdefined" in ss ) {
+    if ("isApplicationInstalled" in navigator && "onloadcssdefined" in ss || 
+    	/PhantomJS/.test(window.navigator.userAgent) && "onloadcssdefined" in ss) {
 		ss.onloadcssdefined( callback );
 	}
 }

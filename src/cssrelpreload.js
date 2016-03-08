93f3d252ea47902c47feec528cca3f537d0b7713
Christopher Meyers <chris@hirechrismeyers.com>
2016-03-08 16:55:36 -0600

    adds check for data-loadcss on rel=preload links

--- src/cssrelpreload.js
+++ src/cssrelpreload.js
@@ -18,7 +18,9 @@
     var links = w.document.getElementsByTagName( "link" );
     for( var i = 0; i < links.length; i++ ){
       var link = links[ i ];
-      if( link.rel === "preload" && link.getAttribute( "as" ) === "style" ){
+      if( link.rel === "preload" &&
+          link.getAttribute( "as" ) === "style" &&
+          link.dataset.loadcss !== "false" ){
         w.loadCSS( link.href, link );
         link.rel = null;
       }

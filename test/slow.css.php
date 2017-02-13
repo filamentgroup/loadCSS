0005f40e5d3d2d91b2c069f8541afdad572724e7
Scott Jehl <scottjehl@gmail.com>
2017-02-13 15:29:36 -0600

    sleep function in demo

--- test/slow.css.php
+++ test/slow.css.php
@@ -1,6 +1,6 @@
 <?php
-header( "Content-type: text/css; charset: UTF-8" );
 sleep(5);
+header( "Content-type: text/css; charset: UTF-8" );
 ?>
 /* This file was delivered after a purposeful 5 second delay to demonstrate latency. */
 body { background: green; color: #fff; }

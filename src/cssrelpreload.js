7b6bdef5f124804c6e1c45baca58b450f880e3dc
Scott Jehl <scottjehl@gmail.com>
2017-10-16 11:08:32 -0500

    update relpreload to retain api

--- src/cssrelpreload.js
+++ src/cssrelpreload.js
@@ -6,8 +6,13 @@
 */
 (function( w ){
   "use strict";
+  // rel=preload support test
+  if( !w.loadCSS ){
+    w.loadCSS = function(){};
+  }
+  var rp = loadCSS.relpreload = {};
   // rel=preload feature support test
-  function preloadSupported(){
+  rp.support = function(){
     try {
       return w.document.createElement( "link" ).relList.supports( "preload" );
     } catch (e) {
@@ -16,7 +21,7 @@
   };
 
   // function to enable a stylesheet so it applies
-  function enableStylesheet( link, media ){
+  rp.enableStylesheet = function( link, media ){
     link.rel = "stylesheet";
     link.media = media;
     // set a localStorage flag
@@ -25,39 +30,42 @@
         w.localStorage[ link.href ] = true;
       } catch( e ){}
     }
-  }
+  };
 
   // loop through link elements in DOM
-  var links = w.document.getElementsByTagName( "link" );
-  for( var i = 0; i < links.length; i++ ){
-    var link = links[ i ];
-    // qualify links to those with rel=preload and as=style attrs
-    if( link.rel === "preload" && link.getAttribute( "as" ) === "style" ){
-      // remember existing media attr for ultimate state, or default to 'all'
-      var finalMedia = link.media || "all";
-      // remember onload attr, if set, to call it later
-      var priorOnload = link.onload;
-      // if preload isn't supported, get an asynchronous load by using a non-matching media attribute
-      // then change that media back to its intended value on load
-      link.onload = function(){
-        enableStylesheet( link, finalMedia );
-        if( priorOnload ){
-          priorOnload.call( link );
+  rp.poly = function(){
+    var links = w.document.getElementsByTagName( "link" );
+    for( var i = 0; i < links.length; i++ ){
+      var link = links[ i ];
+      // qualify links to those with rel=preload and as=style attrs
+      if( link.rel === "preload" && link.getAttribute( "as" ) === "style" ){
+        // remember existing media attr for ultimate state, or default to 'all'
+        var finalMedia = link.media || "all";
+        // remember onload attr, if set, to call it later
+        var priorOnload = link.onload;
+        // if preload isn't supported, get an asynchronous load by using a non-matching media attribute
+        // then change that media back to its intended value on load
+        link.onload = function(){
+          rp.enableStylesheet( link, finalMedia );
+          if( priorOnload ){
+            priorOnload.call( link );
+          }
+        };
+        // if a localStorage flag claims we've loaded/cached this stylesheet already, enable it synchronously now
+        if( w.localStorage && w.localStorage[ link.href ] ){
+          return rp.enableStylesheet( link, finalMedia );
         }
-      };
-      // if a localStorage flag claims we've loaded/cached this stylesheet already, enable it synchronously now
-      if( w.localStorage && w.localStorage[ link.href ] ){
-        return enableStylesheet( link, finalMedia );
+        // if preload is not supported, try to load asynchronously by using a non-matching media query
+        if( !rp.support() ){
+          link.media = "x";
+          link.rel = "stylesheet";
+        }
+        // supported or not, set rel=preload to stylesheet after 3 seconds
+        setTimeout( function(){
+          rp.enableStylesheet(link, finalMedia );
+        }, 3000 );
       }
-      // if preload is not supported, try to load asynchronously by using a non-matching media query
-      if( !preloadSupported() ){
-        link.media = "x";
-        link.rel = "stylesheet";
-      }
-      // supported or not, set rel=preload to stylesheet after 3 seconds
-      setTimeout( function(){
-        enableStylesheet(link, finalMedia );
-      }, 3000 );
     }
-  }
+  };
+  rp.poly();
 })(this);

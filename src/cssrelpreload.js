d63388994dc2071dc199ac8349bcb4645942d0c7
Scott Jehl <scottjehl@gmail.com>
2017-10-16 10:48:55 -0500

    update cssrelpreload.js to a standalone workflow, independent of loadCSS

--- src/cssrelpreload.js
+++ src/cssrelpreload.js
@@ -1,11 +1,13 @@
-/*! loadCSS rel=preload polyfill. [c]2017 Filament Group, Inc. MIT License */
+/*! loadCSS. [c]2017 Filament Group, Inc. MIT License */
+/* This file is meant as a standalone workflow for:
+- testing support for link[rel=preload]
+- enabling async CSS loading in browsers that do not support rel=preload
+- synchronously activating preload stylesheets that have been cached already
+*/
 (function( w ){
-  // rel=preload support test
-  if( !w.loadCSS ){
-    return;
-  }
-  var rp = loadCSS.relpreload = {};
-  rp.support = function(){
+  "use strict";
+  // rel=preload feature support test
+  function preloadSupported(){
     try {
       return w.document.createElement( "link" ).relList.supports( "preload" );
     } catch (e) {
@@ -13,32 +15,49 @@
     }
   };
 
-  // loop preload links and fetch using loadCSS
-  rp.poly = function(){
-    var links = w.document.getElementsByTagName( "link" );
-    for( var i = 0; i < links.length; i++ ){
-      var link = links[ i ];
-      if( link.rel === "preload" && link.getAttribute( "as" ) === "style" ){
-        w.loadCSS( link.href, link, link.getAttribute( "media" ) );
-        link.rel = null;
-      }
-    }
-  };
-
-  // if link[rel=preload] is not supported, we must fetch the CSS manually using loadCSS
-  if( !rp.support() ){
-    rp.poly();
-    var run = w.setInterval( rp.poly, 300 );
-    if( w.addEventListener ){
-      w.addEventListener( "load", function(){
-        rp.poly();
-        w.clearInterval( run );
-      } );
-    }
-    if( w.attachEvent ){
-      w.attachEvent( "onload", function(){
-        w.clearInterval( run );
-      } );
+  // function to enable a stylesheet so it applies
+  function enableStylesheet( link, media ){
+    link.rel = "stylesheet";
+    link.media = media;
+    // set a localStorage flag
+    if( w.localStorage ){
+      try {
+        w.localStorage[ link.href ] = true;
+      } catch( e ){}
     }
   }
-}( this ));
+
+  // loop through link elements in DOM
+  var links = w.document.getElementsByTagName( "link" );
+  for( var i = 0; i < links.length; i++ ){
+    var link = links[ i ];
+    // qualify links to those with rel=preload and as=style attrs
+    if( link.rel === "preload" && link.getAttribute( "as" ) === "style" ){
+      // remember existing media attr for ultimate state, or default to 'all'
+      var finalMedia = link.media || "all";
+      // remember onload attr, if set, to call it later
+      var priorOnload = link.onload;
+      // if preload isn't supported, get an asynchronous load by using a non-matching media attribute
+      // then change that media back to its intended value on load
+      link.onload = function(){
+        enableStylesheet( link, finalMedia );
+        if( priorOnload ){
+          priorOnload.call( link );
+        }
+      };
+      // if a localStorage flag claims we've loaded/cached this stylesheet already, enable it synchronously now
+      if( w.localStorage && w.localStorage[ link.href ] ){
+        return enableStylesheet( link, finalMedia );
+      }
+      // if preload is not supported, try to load asynchronously by using a non-matching media query
+      if( !preloadSupported() ){
+        link.media = "x";
+        link.rel = "stylesheet";
+      }
+      // supported or not, set rel=preload to stylesheet after 3 seconds
+      setTimeout( function(){
+        enableStylesheet(link, finalMedia );
+      }, 3000 );
+    }
+  }
+})(this);

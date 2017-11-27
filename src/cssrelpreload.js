6e119ab59c4b67a37545b973cc4193ae1a4f0e6d
Scott Jehl <scottjehl@gmail.com>
2017-11-27 10:44:34 -0600

    tabby

--- src/cssrelpreload.js
+++ src/cssrelpreload.js
@@ -5,73 +5,73 @@
 - applying rel preload css once loaded, whether supported or not.
 */
 (function( w ){
-  "use strict";
-  // rel=preload support test
-  if( !w.loadCSS ){
-    w.loadCSS = function(){};
-  }
-  // define on the loadCSS obj
-  var rp = loadCSS.relpreload = {};
-  // rel=preload feature support test
-  rp.support = function(){
-    try {
-      return w.document.createElement( "link" ).relList.supports( "preload" );
-    } catch (e) {
-      return false;
-    }
-  };
+	"use strict";
+	// rel=preload support test
+	if( !w.loadCSS ){
+		w.loadCSS = function(){};
+	}
+	// define on the loadCSS obj
+	var rp = loadCSS.relpreload = {};
+	// rel=preload feature support test
+	rp.support = function(){
+		try {
+			return w.document.createElement( "link" ).relList.supports( "preload" );
+		} catch (e) {
+			return false;
+		}
+	};
 
-  // loop through link elements in DOM
-  rp.poly = function(){
-    if( rp.support() ){
-      return;
-    }
-    var links = w.document.getElementsByTagName( "link" );
-    for( var i = 0; i < links.length; i++ ){
-      var link = links[ i ];
-      // qualify links to those with rel=preload and as=style attrs
-      if( link.rel === "preload" && link.getAttribute( "as" ) === "style" && !link.getAttribute( "data-loadcss" ) ){
-        // remember existing media attr for ultimate state, or default to 'all'
-        var finalMedia = link.media || "all";
-        // if preload isn't supported, get an asynchronous load by using a non-matching media attribute
-        // then change that media back to its intended value on load
-        var enableStylesheet = function(){
-          link.media = finalMedia;
-        }
-        if( link.addEventListener ){
-          link.addEventListener( "load", enableStylesheet );
-        } else if( link.attachEvent ){
-          link.attachEvent( "onload", enableStylesheet );
-        }
-        // if preload is not supported, kick off an asynchronous request by using a non-matching media query and rel=stylesheet
-        link.media = "x";
-        link.rel = "stylesheet";
-        // set rel=preload to stylesheet after 3 seconds,
-        // which will catch very old browsers (android 2.x, old firefox) that don't support onload on link
-        setTimeout( enableStylesheet, 3000 );
-        // prevent rerunning on link
-        link.setAttribute( "data-loadcss", true );
-      }
-    }
-    // rerun poly on an interval until onload
-    var run = w.setInterval( rp.poly, 300 );
-    if( w.addEventListener ){
-      w.addEventListener( "load", function(){
-        rp.poly();
-        w.clearInterval( run );
-      } );
-    } else if( w.attachEvent ){
-      w.attachEvent( "onload", function(){
-        rp.poly();
-        w.clearInterval( run );
-      } );
-    }
-  };
-  // run once at least
-  rp.poly();
-  // commonjs
+	// loop through link elements in DOM
+	rp.poly = function(){
+		if( rp.support() ){
+			return;
+		}
+		var links = w.document.getElementsByTagName( "link" );
+		for( var i = 0; i < links.length; i++ ){
+			var link = links[ i ];
+			// qualify links to those with rel=preload and as=style attrs
+			if( link.rel === "preload" && link.getAttribute( "as" ) === "style" && !link.getAttribute( "data-loadcss" ) ){
+				// remember existing media attr for ultimate state, or default to 'all'
+				var finalMedia = link.media || "all";
+				// if preload isn't supported, get an asynchronous load by using a non-matching media attribute
+				// then change that media back to its intended value on load
+				var enableStylesheet = function(){
+					link.media = finalMedia;
+				}
+				if( link.addEventListener ){
+					link.addEventListener( "load", enableStylesheet );
+				} else if( link.attachEvent ){
+					link.attachEvent( "onload", enableStylesheet );
+				}
+				// if preload is not supported, kick off an asynchronous request by using a non-matching media query and rel=stylesheet
+				link.media = "x";
+				link.rel = "stylesheet";
+				// set rel=preload to stylesheet after 3 seconds,
+				// which will catch very old browsers (android 2.x, old firefox) that don't support onload on link
+				setTimeout( enableStylesheet, 3000 );
+				// prevent rerunning on link
+				link.setAttribute( "data-loadcss", true );
+			}
+		}
+		// rerun poly on an interval until onload
+		var run = w.setInterval( rp.poly, 300 );
+		if( w.addEventListener ){
+			w.addEventListener( "load", function(){
+				rp.poly();
+				w.clearInterval( run );
+			} );
+		} else if( w.attachEvent ){
+			w.attachEvent( "onload", function(){
+				rp.poly();
+				w.clearInterval( run );
+			} );
+		}
+	};
+	// run once at least
+	rp.poly();
+	// commonjs
 	if( typeof exports !== "undefined" ){
-    exports.loadCSS = loadCSS;
+		exports.loadCSS = loadCSS;
 	}
 	else {
 		w.loadCSS = loadCSS;

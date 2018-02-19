1e869ab38488e13530f761a4d39ce0c9d2fed6ad
Scott Jehl <scottjehl@users.noreply.github.com>
2018-02-19 16:29:01 -0600

    see if any style change causes that lighthouse warning. #264

--- test/slow.css.php
+++ test/slow.css.php
@@ -3,4 +3,3 @@ sleep(5);
 header( "Content-type: text/css; charset: UTF-8; Cache-Control: max-age=500" );
 ?>
 /* This file was delivered after a purposeful 5 second delay to demonstrate latency. */
-body:after { content: "Slow.css.php has loaded!"; }

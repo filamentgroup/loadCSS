69b79ab7358d21733a022996c886553025c7ca6a
Christian Vetter <christian@rasenplanscher.info>
2019-04-05 18:00:33 +0200

    Prevent early onloadcss callback on old Android

--- src/onloadCSS.js
+++ src/onloadCSS.js
@@ -7,15 +7,26 @@ function onloadCSS( ss, callback ) {
 		return;
 	}
 
-	var called;
-	function loadEventHandler() {
-		if ( called ) {
-			return;
-		}
+	// LEGACY/
+	// This code is for browsers that don’t support onload
+	// No support for onload (it'll bind but never fire):
+	// * Android 4.3 (Samsung Galaxy S4, Browserstack)
+	// * Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
+	// * Android 2.3 (Pantech Burst P9070)
 
-		called = true;
-		callback.call( ss );
+	// Weak inference target for Android < 4.4
+	if ( "isApplicationInstalled" in navigator ) {
+		onloadCSSOldAndroid( ss, callback );
+	} else {
+		onloadCSSLegacy( ss, callback );
 	}
+}
+
+function onloadCSSLegacy( ss, callback ) {
+
+	// /LEGACY
+
+	var called;
 
 	ss.addEventListener( "load", loadEventHandler ); // MODERN
 
@@ -24,20 +35,41 @@ function onloadCSS( ss, callback ) {
 		ss.addEventListener( "load", loadEventHandler );
 	} else if ( ss.attachEvent ) {
 		ss.attachEvent( "onload", loadEventHandler );
-	}
-
-	// This code is for browsers that don’t support onload
-	// No support for onload (it'll bind but never fire):
-	// * Android 4.3 (Samsung Galaxy S4, Browserstack)
-	// * Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
-	// * Android 2.3 (Pantech Burst P9070)
-
-	// Weak inference target for Android < 4.4
-	if ( "isApplicationInstalled" in navigator && "onloadcsslinked" in ss ) {
-		ss.onloadcsslinked( loadEventHandler );
 	}//
 	// /LEGACY
+
+	function loadEventHandler() {
+		if ( called ) {
+			return;
+		}
+
+		called = true;
+		callback.call( ss );
+	}
 }
+
+// LEGACY/
+/**
+ * This is for browsers that don’t support onload.
+ * They will instead fire the callback after a copious timeout
+ * to be reasonably sure that the styles are available.
+ *
+ * No support for onload (it'll bind but never fire):
+ * * Android 4.3 (Samsung Galaxy S4, Browserstack)
+ * * Android 4.2 Browser (Samsung Galaxy SIII Mini GT-I8200L)
+ * * Android 2.3 (Pantech Burst P9070)
+ */
+function onloadCSSOldAndroid( ss, callback ) {
+	if ( "onloadcsslinked" in ss ) {
+		ss.onloadcsslinked( loadEventHandler );
+	}
+
+	function loadEventHandler() {
+		setTimeout( callback, 3000 );
+	}
+}
+
+// /LEGACY
 
 window.onloadCSS = onloadCSS; // GLOBAL
 } )( typeof global !== "undefined" ? global : this ); // GLOBAL
